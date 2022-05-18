658b28d71bb6b630ffd6388ce9c20ccf13ea7a51
Mijail Q. Mariano <mijail.mariano@gmail.com>
2022-05-18 17:09:33 -0400

    updated select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,7 @@
--- Create a new file called select_exercises.sql. Store your code for this exercise in that file. You should be testing your code in MySQL Workbench as you go
--- Use the albums_db database.
+-- 1. Create a new file called select_exercises.sql. Store your code for this exercise in that file. You should be testing your code in MySQL Workbench as you go
+-- 2. Use the albums_db database.
 use albums_db;
--- Explore the structure of the albums table.
+-- 3. Explore the structure of the albums table.
 show tables;
 	-- a. How many rows are in the albums table?
 describe albums;
@@ -21,7 +21,7 @@ describe albums;
 		-- 1967 is the oldest album release date
         -- 2011 is the most recent album release date
         
--- Write queries to find the following information:
+-- 4. Write queries to find the following information:
 	-- a. The name of all albums by Pink Floyd
 select count(distinct(name)) from albums where artist = "Pink Floyd";
 select distinct(name) from albums where artist = "Pink Floyd";
@@ -37,47 +37,47 @@ select genre from albums where name = "Nevermind";
         
 	-- d. Which albums were released in the 1990s
 select distinct(name) from albums where release_date between 1990 and 1999;
--- albums released in the 1990's include: 
-	-- 'Come On Over'
-	-- 'Dangerous'
-	-- 'Falling into You'
-	-- 'Jagged Little Pill'
-	-- 'Let\'s Talk About Love'
-	-- 'Metallica'
-	-- 'Nevermind'
-	-- 'Supernatural'
-	-- 'The Bodyguard'
-	-- 'The Immaculate Collection'
-	-- 'Titanic: Music from the Motion Picture'
+		-- albums (11x) released in the 1990's include: 
+			-- 'Come On Over'
+			-- 'Dangerous'
+			-- 'Falling into You'
+			-- 'Jagged Little Pill'
+			-- 'Let\'s Talk About Love'
+			-- 'Metallica'
+			-- 'Nevermind'
+			-- 'Supernatural'
+			-- 'The Bodyguard'
+			-- 'The Immaculate Collection'
+			-- 'Titanic: Music from the Motion Picture'
 
 	-- e. Which albums had less than 20 million certified sales
 select distinct(name) from albums where sales < 20;
--- albums with less than 20 million certified sales include:
-	-- 'Grease: The Original Soundtrack from the Motion Picture'
-	-- 'Bad'
-	-- 'Sgt. Pepper\'s Lonely Hearts Club Band'
-	-- 'Dirty Dancing'
-	-- 'Let\'s Talk About Love'
-	-- 'Dangerous'
-    -- 'The Immaculate Collection'
-	-- 'Abbey Road'
-	-- 'Born in the U.S.A.'
-	-- 'Brothers in Arms'
-	-- 'Titanic: Music from the Motion Picture'
-	-- 'Nevermind'
-	-- 'The Wall'
+		-- albums (13x) with less than 20 million certified sales include:
+			-- 'Grease: The Original Soundtrack from the Motion Picture'
+			-- 'Bad'
+			-- 'Sgt. Pepper\'s Lonely Hearts Club Band'
+			-- 'Dirty Dancing'
+			-- 'Let\'s Talk About Love'
+			-- 'Dangerous'
+			-- 'The Immaculate Collection'
+			-- 'Abbey Road'
+			-- 'Born in the U.S.A.'
+			-- 'Brothers in Arms'
+			-- 'Titanic: Music from the Motion Picture'
+			-- 'Nevermind'
+			-- 'The Wall'
 
 	-- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 select distinct(name) from albums where genre = "Rock";
--- a. 'Rock' genre albums include:
-	-- '1'
-	-- 'Abbey Road'
-	-- 'Born in the U.S.A.'
-	-- 'Sgt. Pepper\'s Lonely Hearts Club Band'
-	-- 'Supernatural'
--- b. these query results do not return 'hard rock' or 'progressive rock' because we are scripting the query to return values with an exact string.
--- b. [continued] if we wanted to return all values that contained the string 'Rock' as genre than we would code a <like> "%Rock" command instead of equals string.
+		-- a. 'Rock' genre albums (5x) include:
+			-- '1'
+			-- 'Abbey Road'
+			-- 'Born in the U.S.A.'
+			-- 'Sgt. Pepper\'s Lonely Hearts Club Band'
+			-- 'Supernatural'
+		-- b. these query results do not return 'hard rock' or 'progressive rock' because we are scripting the query to return values with an exact string.
+		-- b. [continued] if we wanted to return all values that contained the string 'Rock' as genre than we would code a <like> "%Rock" command instead of equals string.
 
--- Be sure to add, commit, and push your work.
+-- 5. Be sure to add, commit, and push your work.
 
 
